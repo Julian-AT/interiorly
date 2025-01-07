6f037eff137a8af0741f3e1ab45696da04b238d9
Julian S. <200318@studierende.htl-donaustadt.at>
2025-01-07 02:17:48 +0100

    chore: add empty tests
    Co-Authored-By: Anthoy <107678194+An-thonny@users.noreply.github.com>

---
+++ apps/app/__tests__/sign-in.test.tsx
@@ -0,0 +1,5 @@
+import { test } from 'vitest';
+
+test('Sign In Page', () => {
+  // TODO: Add tests
+});
---
+++ apps/app/__tests__/sign-up.test.tsx
@@ -0,0 +1,5 @@
+import { test } from 'vitest';
+
+test('Sign Up Page', () => {
+  // TODO: Add tests
+});
